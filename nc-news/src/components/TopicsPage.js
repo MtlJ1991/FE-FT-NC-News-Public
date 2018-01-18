ade4b8c8621fec4ad22281e0afdb3537708eeb1e
MtlJ1991 <MtlJ1991@gmail.com>
2018-01-18 17:41:12 +0000

    symbols added instead of placeholder names

--- nc-news/src/components/TopicsPage.js
+++ nc-news/src/components/TopicsPage.js
@@ -26,12 +26,12 @@ class TopicsPage extends React.Component {
                     <p className="card-text" key={article._id}>{article.body}</p>
                   </div>
                   <div className="card-header" key={article._id}>
-                    <span><p className="card-title" key={article._id}>Author:  {article.created_by}</p>
-                      <p className="card-title" key={article._id}>Comments:  {article.comments}</p>
-                      <p className="card-title" key={article._id}>Votes:  {article.votes}</p>
+                    <span><p className="card-title" key={article._id}><i class="fa fa-user fa-1g" aria-hidden="true"></i>   :  {article.created_by}</p>
+                      <p className="card-title" key={article._id}><i class="fa fa-comments fa-1g" aria-hidden="true"></i>   :  {article.comments}</p>
+                      <p className="card-title" key={article._id}><i class="fa fa-heart fa-1g" aria-hidden="true"></i>   :  {article.votes}</p>
                       <i className='fa fa-angle-up fa-1g up' aria-hidden="true" onClick={ () => changeTopicVote(this.props.match.params.topic,article._id, 'up').then((body) => {this.setState({articles: body.articles})})}></i>
                       <i className='fa fa-angle-down fa-1g down' aria-hidden="true" onClick={ () => changeTopicVote(this.props.match.params.topic,article._id, 'down').then((body) => {this.setState({articles: body.articles})})}></i>
-                      <p className="card-title" key={article._id}>Topic:  {article.belongs_to}</p>
+                      <p className="card-title" key={article._id}><i class="fa fa-question fa-1g" aria-hidden="true"></i>   :  {article.belongs_to}</p>
 
                       </span>
 
