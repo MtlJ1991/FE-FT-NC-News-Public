7580f169a2da79b2916a95c2d2b78b327dab5ae8
MtlJ1991 <MtlJ1991@gmail.com>
2018-02-13 16:09:17 +0000

    nav bar full restyle and responsive design

--- nc-news/src/components/NavBar.js
+++ nc-news/src/components/NavBar.js
@@ -5,20 +5,16 @@ const NavBar = () => {
 
   return (
     <div className='navigation'>
-      <nav className="navbar navbar-toggleable-md navbar navbar-light bg-light fixed-top">
-        <a className="navbar-brand" href="ncimg"> <img src="https://northcoders.com/images/logos/learn_to_code_manchester_original_second.png" width="450" height="90" alt=""/></a>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-                
-        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-          <div className="navbar-nav">
-            <a className="nav-item nav-link" href="/" style={{'align':'left'}}>Home <span className="sr-only">(current)</span></a>
-            <a className="nav-item nav-link" href="/football">Football articles</a>
-            <a className="nav-item nav-link" href="/cooking">Cooking articles</a>
-            <a className="nav-item nav-link" href="/coding">Coding articles</a>
-          </div>
+      <nav className="navbarNav">
+        <a className="navimg"> <img src="https://northcoders.com/images/logos/learn_to_code_manchester_original_second.png"alt=""/></a>
+        
+        <div className="navbar">
+          <a className="nav-item" href="/" style={{'align':'left'}}>Home <span className="sr-only">(current)</span></a>
+          <a className="nav-item" href="/football">Football articles</a>
+          <a className="nav-item" href="/cooking">Cooking articles</a>
+          <a className="nav-item" href="/coding">Coding articles</a>
         </div>
+        
       </nav>
     </div>
   );
