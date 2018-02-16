3e309f47a54cc26b1c56fb6b6417e516f60b62f1
MtlJ1991 <MtlJ1991@gmail.com>
2018-02-16 14:29:33 +0000

    react smoothScroll added

--- src/App.js
+++ src/App.js
@@ -7,13 +7,21 @@ import UsersPage from './components/UsersPage'
 import TopicsPage from './components/TopicsPage'
 import CommentsPage from './components/CommentsPage'
 import { BrowserRouter, Route } from "react-router-dom";
+import ReallySmoothScroll from 'really-smooth-scroll';
 
+ReallySmoothScroll.shim();
+
+ReallySmoothScroll.config({
+  mousewheelSensitivity: 9, // Default 
+  keydownSensitivity: 9 // Default (When you press arrow down/up key) 
+});
 
 class App extends Component {
 
   state = {
     articles: []
   }
+
 
   render() {
     return (
